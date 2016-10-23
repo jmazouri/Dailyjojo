5b6a82d8bb3aa7a2b9e3f6b850364108214fe556
John Mazouri <jmazouri@users.noreply.github.com>
2016-10-23 19:53:28 -0400

    More videos & greato days

--- jojo.js
+++ jojo.js
@@ -1,9 +1,18 @@
-var videos = ["8w9LYL0sMx4", "SgWcSuxyvT8", "o7aW0Uj3778", "vgLbcuDhmzo",
+var videos = ["8w9LYL0sMx4", "SgWcSuxyvT8", "vgLbcuDhmzo", "6BgqFYrD8UM",
               "BEQMXjN8mxU", "3P7qWQ5dBmA", "YWbkUKtogB4", "yacOhxLutX4",
               "tyzt4N0XDUM", "uFqjIOWs8Cg", "7I6OtXYXv20", "hWKB1Zxg84s",
               "hS3nur6o6ME", "dZHMuUxuUfQ", "FwEQlJAYh3c", "tGhiCs6QRKw",
               "t2Gkc7LgoQ8", "jTCygHGhu78", "6zZlQ1WSn5U", "4KNqFhJ6-n0",
-              "slxjvldJ3jg", "6BgqFYrD8UM"];
+              "slxjvldJ3jg", "75Q440WphbE", "KQ5kRyzQkyw", "8OOA2qElunM",
+              "Vg7qEpiZ5uU", "dBDbwyVYrfk", "rZJa1gP_akA"];
+
+var titleVideos = ["kkLgIrdIEfA", "iZ7Y5IqIHSk"];
+var chosenTitle = titleVideos[0];
+
+if (Math.random() > 0.8)
+{
+    chosenTitle = titleVideos[1];
+}
 
 var lastVideos = [];
 var player;
@@ -15,7 +24,7 @@ function onYouTubeIframeAPIReady()
     {
         //height: '720',
         //width: '1280',
-        videoId: 'iZ7Y5IqIHSk',
+        videoId: chosenTitle,
         events:
         {
             'onReady': onPlayerReady,
@@ -29,12 +38,12 @@ function onYouTubeIframeAPIReady()
             'modestbranding': 1
         }
     });
-   
+
 }
 
 function onPlayerReady(event)
 {
-    
+
 }
 
 function onPlayerStateChange(event)
@@ -49,11 +58,11 @@ function playNext()
 {
     player.loadVideoById("4NsqFlZhlSo");
     player.setVolume(25);
-    
+
     var curTimeout = setTimeout(function()
     {
         var videoList = _.difference(videos, lastVideos);
-        
+
         if (videoList.length == 0)
         {
             alert("JOJO RESERVES ARE DEPLETED. REFRESH TO RELIVE THE EXPERIENCE.");
@@ -61,11 +70,10 @@ function playNext()
             keepGoing = false;
             return;
         }
-        
+
         var selectedVideo = videoList[Math.floor(Math.random() * videoList.length)];
-        
+
         lastVideos.push(selectedVideo);
         player.loadVideoById(selectedVideo);
     }, 1000);
 }
-
